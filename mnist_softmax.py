8a542c7c334a40b14516adbcd944a98d5c2ac058
Sujay Vittal <sujayvittal@gmail.com>
2017-03-03 23:57:52 +0530

    mnist_softmax.py
    Updated accuracy (in Percentage)

--- mnist_softmax.py
+++ mnist_softmax.py
@@ -47,7 +47,8 @@ def main(_):
 
   # Test trained model
   correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-  accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  accuracy-prob = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+  accuracy = accuracy-prob*100
   print(sess.run(accuracy, feed_dict={x: mnist.test.images,
                                       y_: mnist.test.labels}))
 
